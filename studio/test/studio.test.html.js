e5966b1dd4169c39f024ac759e9bfdcd711f3429
Ilyas Türkben <ilyas@adobe.com>
2024-09-16 16:31:58 +0200

    fix test

--- studio/test/studio.test.html.js
+++ studio/test/studio.test.html.js
@@ -9,7 +9,6 @@ import '../src/studio.js';
 import { mockFetch } from '@adobecom/milo/libs/features/mas/mocks/fetch.js';
 import { withAem } from '@adobecom/milo/libs/features/mas/mocks/aem.js';
 import { withWcs } from '@adobecom/milo/libs/features/mas/mocks/wcs.js';
-import { withLiterals } from '@adobecom/milo/libs/features/mas/mocks/literals.js';
 
 import { getTemplateContent, delay } from '@adobe/mas-commons/test/utils.js';
 
@@ -26,24 +25,6 @@ runTests(async () => {
             const [studio] = getTemplateContent('studio');
             document.querySelector('main').append(studio);
             expect(studio).exist;
-        });
-
-        it.skip('should search via deeplink', async () => {
-            // const disposer = await mockFetch(withLiterals, withWcs, withAem);
-            const disposer = await mockFetch(withLiterals, withWcs);
-            document.location.hash =
-                '#path=%2Fcontent%2Fdam%2Fsandbox%2Fmas&query=ccd';
-            const [studio] = getTemplateContent('studio');
-            document.querySelector('main').append(studio);
-            await delay(100);
-            const [cc, photoshop] = document.querySelectorAll('merch-card');
-            expect(cc).to.exist;
-            expect(photoshop).to.exist;
-            cc.dispatchEvent(new Event('dblclick'));
-            await studio.updateComplate;
-            const editor = document.getElementById('editor');
-            await delay(100);
-            expect(editor.innerText).to.not.empty;
         });
     });
 });
