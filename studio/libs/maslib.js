7bea90d4920a091974cf9242cd5ef769a78a44fc
Nicolas Peltier <1032754+npeltier@users.noreply.github.com>
2024-09-24 18:22:01 +0200

    MWPW-159027 introduce milolibs in studio (#58)
    - whatever milo branch for mas.js can be switched to with milolibs parameter,
    - you can switch to local dev with 'local' value

---
+++ studio/libs/maslib.js
@@ -0,0 +1,15 @@
+const getMiloLibs = () => {
+    const milolibs = new URLSearchParams(window.location.search)?.get('milolibs');
+    if (!milolibs) return 'https://www.adobe.com';
+    if ('local' === milolibs) return 'http://localhost:6456';
+    return `https://${milolibs}.hlx.live`;
+}
+
+const injectMasLib = () => {
+    const script = document.createElement('script');
+    script.setAttribute("src", `${getMiloLibs()}/libs/deps/mas/mas.js`);
+    script.setAttribute("type", 'module');
+    document.head.prepend(script);
+};
+
+injectMasLib();
