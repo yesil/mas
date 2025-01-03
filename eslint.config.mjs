d8aad0cb8f17c8716ac8fe4f52cfe84d5794943b
Mariia Lukianets <lukianet@adobe.com>
2025-01-03 14:58:56 +0100

    Revert "MWPW-164253: move products.js from Web Stage to MAS (#124)" (#127)
    This reverts commit 9042b96c4aef363ac25f58dff953fd5ff68fded1.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,8 +14,6 @@ const config = {
         '/libs/',
         '**/libs/**',
         '**/dist/**',
-        'studio/ost/products.js',
-        'studio/ost/index.js',
     ],
     ...eslintPluginPrettierRecommended,
 };
