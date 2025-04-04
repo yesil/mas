43d31a61ffa925ea7686481ffe6b0709157a54cf
Sean Choi <seanchoi@adobe.com>
2025-04-04 03:15:52 -0600

    Adding sr-only css for rte-field. (#218)
    Co-authored-by: Mariia Lukianets <lukianet@adobe.com>

--- studio/src/rte/rte-field.js
+++ studio/src/rte/rte-field.js
@@ -231,6 +231,17 @@ class RteField extends LitElement {
                     outline-offset: 2px;
                     border-radius: 16px;
                 }
+                sr-only {
+                    position: absolute;
+                    width: 1px;
+                    height: 1px;
+                    padding: 0;
+                    margin: -1px;
+                    overflow: hidden;
+                    clip: rect(0, 0, 0, 0);
+                    white-space: nowrap;
+                    border: 0;
+                }
             `,
             prosemirrorStyles,
         ];
