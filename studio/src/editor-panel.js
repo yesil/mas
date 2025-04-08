0348aeac601a5fcf1577422f6346b9ea390b6cb9
Angelo Statescu <angelostatescu.adobe@gmail.com>
2025-04-08 15:37:30 +0300

    MWPW-165461: Added content-type to use url (#219)
    * MWPW-165461: Added content-type to use url
    
    * and
    
    * revert test
    
    * re-added

--- studio/src/editor-panel.js
+++ studio/src/editor-panel.js
@@ -225,7 +225,7 @@ export default class EditorPanel extends LitElement {
 
         const code = `<${webComponentName}><aem-fragment fragment="${this.fragment?.id}" title="${title}"></aem-fragment></${webComponentName}>`;
         const richText = `
-                <a href="https://mas.adobe.com/studio.html#page=${Store.page.value}&path=${Store.search.value.path}&query=${this.fragment?.id}">
+                <a href="https://mas.adobe.com/studio.html#content-type=${webComponentName}&page=${Store.page.value}&path=${Store.search.value.path}&query=${this.fragment?.id}">
                     ${webComponentName}: ${fragmentParts}
                 </a>
             `;
