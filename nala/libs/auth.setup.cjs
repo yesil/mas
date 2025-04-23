f8f8a9a1f4ef9804c89260cdd70c022475515ffb
Nicolas Peltier <1032754+npeltier@users.noreply.github.com>
2025-04-23 15:28:41 +0200

    @releng fixing (again) login page selector  (#238)
    * @releng fixing login page selector
    
    * @releng fixing (again) login page selector

--- nala/libs/auth.setup.cjs
+++ nala/libs/auth.setup.cjs
@@ -18,7 +18,7 @@ setup('authenticate, @mas-studio', async ({ page, baseURL, browserName }) => {
   await page.waitForURL('**/auth.services.adobe.com/en_US/index.html**/');
 
   await expect(page).toHaveTitle(/Adobe ID/);
-  let heading = await page.locator('.Heading-1').first().innerText();
+  let heading = await page.locator('.spectrum-Heading1,.Heading-1').first().innerText();
   expect(heading).toBe('Sign in');
 
   // Fill out Sign-in Form
@@ -31,7 +31,7 @@ setup('authenticate, @mas-studio', async ({ page, baseURL, browserName }) => {
     timeout: 10_000,
   });
 
-  heading = await page.locator('.Heading-1', { hasText: 'Enter your password' }).first().innerText();
+  heading = await page.locator('.spectrum-Heading1,.Heading-1', { hasText: 'Enter your password' }).first().innerText();
   expect(heading).toBe('Enter your password');
   await page.locator('#PasswordPage-PasswordField').fill(process.env.IMS_PASS);
   await page.locator('[data-id=PasswordPage-ContinueButton]').click();
