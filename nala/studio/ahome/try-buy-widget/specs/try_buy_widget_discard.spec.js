f4b3907087ff24216b577ecff65f88f6ad6b9adb
Milica Micic <cod23684@adobe.com>
2025-06-17 14:22:10 +0200

    [Nala] update price (#297)
    * prevent monitors on forks
    
    * Nala: update price

--- nala/studio/ahome/try-buy-widget/specs/try_buy_widget_discard.spec.js
+++ nala/studio/ahome/try-buy-widget/specs/try_buy_widget_discard.spec.js
@@ -7,8 +7,8 @@ export default {
             path: '/studio.html',
             data: {
                 cardid: '2d9025f7-ea56-4eeb-81b2-a52762358b9d',
-                price: 'US$79.99/mo',
-                newPrice: 'US$79.99/moper license',
+                price: 'US$89.99/mo',
+                newPrice: 'US$89.99/moper license',
             },
             browserParams: '#query=',
             tags: '@mas-studio @ahome @ahome-try-buy-widget @ahome-try-buy-widget-discard',
--- nala/studio/ahome/try-buy-widget/specs/try_buy_widget_save.spec.js
+++ nala/studio/ahome/try-buy-widget/specs/try_buy_widget_save.spec.js
@@ -7,7 +7,7 @@ export default {
             path: '/studio.html',
             data: {
                 cardid: '2d9025f7-ea56-4eeb-81b2-a52762358b9d',
-                price: 'US$79.99/mo',
+                price: 'US$89.99/mo',
             },
             browserParams: '#query=',
             tags: '@mas-studio @ahome @ahome-try-buy-widget @ahome-try-buy-widget-save',
